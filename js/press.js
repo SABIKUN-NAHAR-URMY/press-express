6ba6a2ac657ceb25af0a37a421e87e536958c52b
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-09-03 12:52:28 +0600

    spinner added done

--- js/press.js
+++ js/press.js
@@ -9,17 +9,23 @@ const loadCategory = () =>
 const displayCategory = (categories) =>
 {
     const categorySection = document.getElementById('category-section');
+    
     categories.forEach(category => {
+       
         const createDiv = document.createElement('div');
         createDiv.innerHTML= `
             <h5 class="p-lg-4" onclick= "categoryNews('${category.category_id}')">${category.category_name}</h5>
         `;
         categorySection.appendChild(createDiv);
+        
     });
 }
 
 const categoryNews = (categoryId) =>
 {
+      //start loader
+    toggleSpinner(true);
+
     const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`
     fetch(url)
     .then(res => res.json())
@@ -28,9 +34,20 @@ const categoryNews = (categoryId) =>
 
 const displayCategoryNews = (news) =>
 {
-    console.log(news);
     const categoryNewsShow = document.getElementById('category-news');
     categoryNewsShow.innerHTML = ``;
+    
+    if(news.length === 0)
+    {
+        const createDiv = document.createElement('div');
+        createDiv.innerHTML = `
+        <h1 class="text-center">No News Found</h1>
+        `;
+        categoryNewsShow.appendChild(createDiv);
+        //finish loader
+        toggleSpinner(false);
+    }
+   else{
     news.forEach(element =>{
         const createDiv = document.createElement('div');
         createDiv.classList.add('card');
@@ -38,20 +55,39 @@ const displayCategoryNews = (news) =>
         createDiv.innerHTML = `
             <div class="row g-0 m-3">
                 <div class="col-md-4">
-                    <img src="${element.image_url}" class="img-fluid rounded-start" alt="...">
+                    <img src="${element.image_url ? element.image_url : "No News Found"}" class="img-fluid rounded-start" alt="...">
                 </div>
                 <div class="col-md-8">
                 <div class="card-body">
                     <h5 class="card-title fw-bold">${element.title}</h5>
-                    <p class="card-text">${element.details}</p>
+                    <p class="card-text text-truncate">${element.details}</p>
                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
                 </div>
             </div>
         `;
         categoryNewsShow.appendChild(createDiv);
+
+        //finish loader
+        toggleSpinner(false);
+
     })
     
+   }
+    
+    
+}
+
+const toggleSpinner = isloading =>{
+    const loaderSection = document.getElementById('loader');
+    if(isloading)
+    {
+        loaderSection.classList.remove('d-none');
+    }
+    else
+    {
+        loaderSection.classList.add('d-none');
+    }
 }
 
 loadCategory();
